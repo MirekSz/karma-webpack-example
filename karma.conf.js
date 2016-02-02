7ebd31cb0a17f00bd53358f3a7d41ba8a7fccdb3
Mirek <m.szajo>
2016-02-02 14:47:38 +0100

    s

--- karma.conf.js
+++ karma.conf.js
@@ -1,66 +1,71 @@
 var webpack = require("webpack");
-module.exports = function(config) {
-  config.set({
+module.exports = function (config) {
+    config.set({
 
-    files: [
-      // all files ending in "test"
-      './node_modules/phantomjs-polyfill/bind-polyfill.js',
-      'test/test.js'
-      // each file acts as entry point for the webpack configuration
-    ],
-
-    // frameworks to use
-    frameworks: ['mocha'],
-
-    preprocessors: {
-      // only specify one entry point
-      // and require all tests in there
-      'test/test.js': ['webpack']
-    },
-
-    reporters: ['spec', 'coverage'],
-
-    coverageReporter: {
-      type: 'html',
-      dir: 'build/coverage/'
-    },
-
-    webpack: {
-      // webpack configuration
-      module: {
-        loaders: [
-          {test: /\.css$/, loader: "style!css"},
-          {test: /\.less$/, loader: "style!css!less"}
+        files: [
+            // all files ending in "test"
+            './node_modules/phantomjs-polyfill/bind-polyfill.js',
+            'test/test.js'
+            // each file acts as entry point for the webpack configuration
         ],
-        postLoaders: [{
-          test: /\.js/,
-          exclude: /(test|node_modules|bower_components)/,
-          loader: 'istanbul-instrumenter'
-        }]
-      },
-      resolve: {
-        modulesDirectories: [
-          "",
-          "src",
-          "node_modules"
-        ]
-      }
-    },
 
-    webpackMiddleware: {
-      // webpack-dev-middleware configuration
-      noInfo: true
-    },
+        // frameworks to use
+        frameworks: ['mocha'],
 
-    plugins: [
-      require("karma-webpack"),
-      require("istanbul-instrumenter-loader"),
-      require("karma-mocha"),
-      require("karma-coverage"),
-      require("karma-phantomjs-launcher"),
-      require("karma-spec-reporter")
-    ],
+        preprocessors: {
+            // only specify one entry point
+            // and require all tests in there
+            'test/test.js': ['webpack']
+        },
 
-    browsers: ['PhantomJS']
-  });
+        reporters: ['spec', 'coverage', 'junit'],
+
+        junitReporter: {
+            outputFile: 'test-results.xml'
+        },
+
+        coverageReporter: {
+            type: 'html',
+            dir: 'build/coverage/'
+        },
+
+        webpack: {
+            // webpack configuration
+            module: {
+                loaders: [
+                    {test: /\.css$/, loader: "style!css"},
+                    {test: /\.less$/, loader: "style!css!less"}
+                ],
+                postLoaders: [{
+                    test: /\.js/,
+                    exclude: /(test|node_modules|bower_components)/,
+                    loader: 'istanbul-instrumenter'
+                }]
+            },
+            resolve: {
+                modulesDirectories: [
+                    "",
+                    "src",
+                    "node_modules"
+                ]
+            }
+        },
+
+        webpackMiddleware: {
+            // webpack-dev-middleware configuration
+            noInfo: true
+        },
+
+        plugins: [
+            require("karma-webpack"),
+            require("istanbul-instrumenter-loader"),
+            require("karma-mocha"),
+            require("karma-coverage"),
+            require("karma-phantomjs-launcher"),
+            require("karma-spec-reporter"),
+            require("karma-junit-reporter")
+        ],
+
+        browsers: ['PhantomJS']
+    });
 };
--- test/Example/hexColorLuminance_test.js
+++ test/Example/hexColorLuminance_test.js
@@ -4,14 +4,15 @@ var expect = chai.expect;
 var hexColorLuminance = require('../../src/Example/hexColorLuminance');
 
 describe("hexColorLuminance", function () {
-  it("should return a luminanced color", function () {
-    var color = hexColorLuminance.luminate("#fff", "-0.5");
-    expect(color).is.not.empty;
-  });
-  it("should change the background of an element", function () {
-    var div = document.createElement('div');
-    div.style.backgroundColor = hexColorLuminance.luminate("#fff", "-0.5");
-    expect(div.style.backgroundColor).is.not.empty;
-  });
+    it("should return a luminanced color", function () {
+        var color = hexColorLuminance.luminate("#fff", "-0.5");
+        expect(color).is.not.empty;
+    });
+
+    it("should change the background of an element", function () {
+        var div = document.createElement('div');
+        div.style.backgroundColor = hexColorLuminance.luminate("#fff", "-0.5");
+        expect(div.style.backgroundColor).is.not.empty;
+    });
 
 });
